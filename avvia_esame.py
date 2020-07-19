630129c689d4bfaae730be4c5380ccbff1e75b6e
Adriano Tumminelli <adriano.tummi@hotmail.it>
2020-07-19 15:31:10 +0200

    Update avvia_esame.py

--- avvia_esame.py
+++ avvia_esame.py
@@ -9,7 +9,7 @@ pythonPath = sys.executable
 
 # start local http server
 p1 = subprocess.Popen(pythonPath + " -m http.server " + webserverPort + " --bind " + webserverIp, shell=True)
-webbrowser.open("http://" + webserverIp + ":" + webserverPort + "/index.html")
+webbrowser.open("http://" + webserverIp + ":" + webserverPort + "/map/index.html")
 
 # start jupyter notebook
 p2 = subprocess.Popen(pythonPath + " -m jupyter notebook --no-browser --port " + jupyterPort, shell=True) 
