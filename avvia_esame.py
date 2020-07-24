cfc1ccb89717911b71c1a41ab67bacb893f90fea
Adriano Tumminelli <adriano.tummi@hotmail.it>
2020-07-25 00:26:23 +0200

    Update avvia_esame.py

--- avvia_esame.py
+++ avvia_esame.py
@@ -4,16 +4,46 @@ import subprocess
 import webbrowser
 from config import * 
 
+conda_environment = "ROexam"
+if len(sys.argv) == 2:
+    conda_environment = sys.argv[1]
+
+conda_installed = True
+try:
+    subprocess.Popen("conda").terminate()
+except:
+    conda_installed = False
+
+
 pythonPath = sys.executable
 
+activate_environment = ""
+if conda_installed:
+    if  sys.platform == "win32":
+        separator = " && "
+    else:
+        separator = " ; "
+    pythonPath = "python"
+    activate_environment = "conda activate " + conda_environment + separator
+else:
+    print("Conda is not installed using the pip version of jupyter notebook")
+
+
 # start local http server
-p1 = subprocess.Popen(pythonPath + " -m http.server " + webserverPort + " --bind " + webserverIp, shell=True)
+command = pythonPath + " -m http.server " + webserverPort + " --bind " + webserverIp
+print("> " + command)
+p1 = subprocess.Popen(command, shell=True)
 webbrowser.open("http://" + webserverIp + ":" + webserverPort + "/map/index.html?port="+jupyterPort)
 
 # stop jupyter notebook
-p2 = subprocess.Popen(pythonPath + " -m notebook stop " + jupyterPort, shell=True)
+command =  activate_environment +  pythonPath + " -m notebook stop " + jupyterPort
+print("> " + command)
+p2 = subprocess.Popen(command, shell=True)
+
 # start jupyter notebook
-p3 = subprocess.Popen(pythonPath + " -m notebook --port " + jupyterPort, shell=True) 
+command = activate_environment +  pythonPath + " -m notebook --port " + jupyterPort
+print("> " + command)
+p3 = subprocess.Popen(command, shell=True) 
 
 try:
     p1.communicate()
@@ -21,4 +51,6 @@ try:
     p3.communicate()
 except KeyboardInterrupt:
     print("\n----Closing program---\n")
-    p4 = subprocess.Popen(pythonPath + " -m notebook stop " + jupyterPort, shell=True)
+    command = activate_environment + pythonPath + " -m notebook stop " + jupyterPort
+    print("> " + command)
+    p4 = subprocess.Popen(command, shell=True)
