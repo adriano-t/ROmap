3fabcada22901b45bae490c363cc8356b5379a16
Adriano Tumminelli <adriano.tummi@hotmail.it>
2020-07-24 19:21:26 +0200

    Update map_generator.py

--- generator/map_generator.py
+++ generator/map_generator.py
@@ -19,38 +19,36 @@ def generate(data):
     with open("templates/task.html", 'r') as f:
         task_template = f.read()
 
-    with open('map.yaml') as f: 
-        for exercise in data: 
+    for exercise in data: 
+        ex_tags = ""
+        for tag in exercise["tags"]: 
+            ex_tags += '<div class="competence">' + tag + '</div> '
+        
+        tot_points = exercise["tot_points"]
 
-            ex_tags = ""
-            for tag in exercise["tags"]: 
-                ex_tags += '<div class="competence">' + tag + '</div> '
-            
-            tot_points = exercise["tot_points"]
+        tasks_html = ""
+        task_idx = 1
+        for task in exercise["tasks"]:
 
-            tasks_html = ""
-            task_idx = 1
-            for task in exercise["tasks"]:
+            task_values = ""
+            for i in range(1, task["tot_points"] + 1):
+                task_values += '<option value="'+str(i)+'">'+str(i)+'</option>\n'
 
-                task_values = ""
-                for i in range(1, task["tot_points"] + 1):
-                    task_values += '<option value="'+str(i)+'">'+str(i)+'</option>\n'
-
-                tasks_html += task_template.format(
-                    ex_task_name = "Task " + html.escape(str(task_idx)),
-                    ex_task_values = task_values,
-                    ex_task_max_points = str(task["tot_points"] ),
-                    ex_task_description = html.escape(task["description1"])
-                ) + "\n"
-                task_idx += 1
-            
-            outHTML += exercise_template.format(
-                ex_title = html.escape(exercise["title"]),
-                ex_tags = ex_tags,
-                ex_tot_points = tot_points,
-                ex_link = exercise["link"],
-                ex_tasks = tasks_html
-            )
+            tasks_html += task_template.format(
+                ex_task_name = "Task " + html.escape(str(task_idx)),
+                ex_task_values = task_values,
+                ex_task_max_points = str(task["tot_points"] ),
+                ex_task_description = html.escape(task["description1"])
+            ) + "\n"
+            task_idx += 1
+        
+        outHTML += exercise_template.format(
+            ex_title = html.escape(exercise["title"]),
+            ex_tags = ex_tags,
+            ex_tot_points = tot_points,
+            ex_link = exercise["link"],
+            ex_tasks = tasks_html
+        )
 
     #html footer
     with open("templates/footer.html", 'r') as f:
